bf11a583ceaedbcaf98143a412fc265cb1024a98
Niklas Schnelle <niklas@komani.de>
2012-10-15 17:18:03 +0200

    Shell Scripts for measuing performance

---
+++ perftests/threads-levels.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+echo 'Algorithm, # Threads, avg. Duration in ms, avg. size in kb, throughput, transferrate in Mbit/s, max Level'
+
+for level in `seq 10 10 120`;
+do
+	for threads in `seq 1 1 16`;
+	do
+		pkill java
+		sed "s/##NUM_THREADS##/$threads/g" < tourenplaner-template.conf > tourenplaner.conf
+		java  -Xmx8g -Xincgc -jar target/tourenplaner-server-1.0-SNAPSHOT-jar-with-dependencies.jar -f dump -c tourenplaner.conf &>out.log &
+		sleep 30	
+		perftester -concurrent 16 -format csv -algorithm updowng -requests 10 -constrained maxSearchLevel -intConstrained $level -server 'http://localhost:8080' &> /dev/null # warm up
+		sleep 1
+		echo -e -n "updowng, $threads, " `perftester -concurrent 16 -format csv -algorithm updowng -constrained maxSearchLevel -intConstrained $level -requests 200 -server 'http://localhost:8080'` ", $level\n"
+	done
+
+done
---
+++ perftests/threads.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+echo '#Algorithm, Num Threads, avg. Duration in ms, avg. size in kb, throughput, transferrate in Mbit/s'
+for threads in `seq 1 16`;
+do
+	pkill java
+	sed "s/##NUM_THREADS##/$threads/g" < tourenplaner-template.conf > tourenplaner.conf
+	java  -Xmx8g -Xincgc -jar target/tourenplaner-server-1.0-SNAPSHOT-jar-with-dependencies.jar -f dump -c tourenplaner.conf &>out.log &
+	sleep 20	
+	perftester -concurrent 16 -format csv -algorithm updowng -requests 20 -server 'http://localhost:8080' &> /dev/null # warm up
+	echo -e -n "updowng , $threads, " `perftester -concurrent 16 -format csv -algorithm updowng -requests 20 -server 'http://localhost:8080'` '\n'
+done
+# Fot Gnuplot datablock seperation
+echo -e -n '\n\n'
+for threads in `seq 1 16`;
+do
+	pkill java
+	sed "s/##NUM_THREADS##/$threads/g" < tourenplaner-template.conf > tourenplaner.conf
+	java  -Xmx8g -Xincgc -jar target/tourenplaner-server-1.0-SNAPSHOT-jar-with-dependencies.jar -f dump -c tourenplaner.conf &>out.log &
+	sleep 20	
+
+	perftester -concurrent 16 -format csv -algorithm sp -requests 20 -server 'http://localhost:8080' &>/dev/null #warm up
+	echo -e -n "sp , $threads, " `perftester -concurrent 16 -format csv -algorithm sp -requests 20 -server 'http://localhost:8080' 2> /dev/null` '\n'
+done
+
